b95122e25ba55220af6b357554518e1437371208
Samuel Asher Rivello <63511769+SamuelAsherRivello@users.noreply.github.com>
2023-10-17 14:18:00 -0300

    Rename folders

---
+++ Unity/Assets/3rdParty/RMC/Demos/Projects/Design Patterns/Creational Patterns/Singleton/ScriptableSingleton/ScriptableSingletonDemo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace RMC.Projects.UnityDesignPatterns.Patterns.CreationalPatterns.Singleton
+{
+	/// <summary>
+	/// Ensure a class has only one instance and provide a global point of access to it.
+	/// See <a href="https://www.dofactory.com/net/singleton-design-pattern">docs</a>.
+	/// </summary>
+	public class ScriptableSingletonDemo : MonoBehaviour
+	{
+		protected void Start()
+		{
+			// Singleton ScriptableObject
+			// -- Writes to local path
+			MyScriptableSingleton.instance.Log();
+			MyScriptableSingleton.instance.IncrementSomeCounter();
+			MyScriptableSingleton.instance.Log();
+		}
+	}
+}
